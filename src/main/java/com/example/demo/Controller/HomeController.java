482d9aa9a0ff22ec35bf06b8537695ebe588bef4
gollam rabby <rabby2186@gmail.com>
2020-08-03 22:10:15 +0200

    get data  - as a link of list/array

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.demo.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -15,38 +17,39 @@ import java.util.Map;
 public class HomeController {
 
     @RequestMapping("/fcp_api")
-    public Map index() {
+    public Map index(@RequestBody List<String> urlPath) {
         //for OntoFarm
-        String OntoFarmCollectionList = "117810";
         //String OntoFarmCollectionList = "117810";
-        String ontologies[] = OntoFarmCollectionList.split(",");
-        Map<String, Object> data = null;
+        //String OntoFarmCollectionList = "117810";
+        //String ontologies[] = OntoFarmCollectionList.split(",");
+        Map<String, Object> data = new HashMap<>();
 
         //System.exit(1);
 
         FCP fcp = new FCP();
-        BufferedReader reader;
+        //BufferedReader reader;
         List list = new ArrayList();
         try {
 
-            for(String s : ontologies) {
+            for(String s : urlPath) {
                 System.out.println(s);
-                data = fcp.getCategorizationOptions("https://owl.vse.cz/ontologies/"+s.trim()+".owl", true);
+                data.put(s,fcp.getCategorizationOptions(s, true));
             }
+
 
             //run for one ontology
             //String onto = "https://owl.vse.cz/ontologies/117810.owl"; //cmt.owl
             //fcp.getCategorizationOptions(onto, true);
 
 
-            reader = new BufferedReader(new FileReader("a.txt"));
+            /*reader = new BufferedReader(new FileReader("a.txt"));
             String line = reader.readLine();
 
             while (line != null) {
                 list.add(line);
                 line = reader.readLine();
             }
-            reader.close();
+            reader.close();*/
         }
         catch (Exception e) {
             e.printStackTrace();
